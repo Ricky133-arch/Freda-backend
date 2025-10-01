4e6dc245bb656f52b71475818406fb866cc970ea
fredrick izidor <izidorfredrick@gmail.com>
2025-10-01 18:51:25 +0100

    Update CORS for new frontend URL https://freda-frontend.onrender.com

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://freda-frontend-bau6.onrender.com'],
+    origin: ['http://localhost:5173', 'https://freda-frontend.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   },
 });
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://freda-frontend-bau6.onrender.com'],
+  origin: ['http://localhost:5173', 'https://freda-frontend.onrender.com'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
